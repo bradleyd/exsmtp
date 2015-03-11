f34efe3d921f20f0dd81b6714a95203ffb2c112e
Bradley Smith <brad.smith@sendgrid.com>
2015-03-11 17:03:20 -0600

    add mix stuff for hex package.  Update readme

--- README.md
+++ README.md
@@ -7,7 +7,24 @@ It uses the wonderful [gen_smtp](https://github.com/Vagabond/gen_smtp)
 There was also code inspired by [HashNuke's Mail To JSON](https://github.com/HashNuke/mail-to-json)
 
 
+## Usage
+
+Include into `mix.exs`
+
+Set the SMTP port either in the config or by environment variable `EXSMTP_SMTP_PORT`
+
+Start in foreground
+
+```elxir
+mix run --no-halt
+
+17:00:26.314 [info]  :gen_smtp_server starting at :nonode@nohost
+
+17:00:26.324 [info]  :gen_smtp_server listening on {0, 0, 0, 0}:2525 via :tcp
+```
+
 ## TODO 
 - [ ] Better tests
 - [ ] Parse email so humans can use it
 - [ ] Benchmark
+- [ ] Scriptize?
--- mix.exs
+++ mix.exs
@@ -5,26 +5,30 @@ defmodule Exsmtp.Mixfile do
     [app: :exsmtp,
       version: "0.0.1",
       elixir: "~> 1.0",
+      description: description,
+      package: package,
       deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger],
       mod: {Exsmtp, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  def description do
+    "Basic SMTP server for Elixir"
+  end
+
+  def package do
+    [
+      contributors: ["Bradley Smith"],
+      licenses: ["The MIT License"],
+      links: %{
+        "GitHub" => "https://github.com/bradleyd/exsmtp"
+      }
+  ]
+  end
+
   defp deps do
     [
       {:eiconv,    github: "zotonic/eiconv"},
